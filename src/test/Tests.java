fd5983c8bfcae3bbe9fbb3554ea8d2b504c4de7b
nuttdguy <pygnasakdev@gmail.com>
2022-07-05 14:46:56 -0500

    Add tests for game

--- src/test/Tests.java
+++ src/test/Tests.java
@@ -1,6 +1,15 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Tests {
 
     Game game;
@@ -18,6 +27,45 @@ public class Tests {
                 "is greedy and hungry and will eat you on sight.\n";
         String actual = game.getGameIntro();
 
+        assertEquals(expected.equals(actual), "Should match expected String");
+    }
+
+    @Test
+    void getPromptQuestionsTest() {
+        String expected = "Which cave will you go into?\n";
+        String actual = game.getPromptQuestion();
+
+        assertEquals(expected.equals(actual), "Should match expected String");
+    }
+
+    @Test
+    void getPromptOptionsTest() {
+        String[] expected = new String[]{"1. You approach the cave...", "2. It is dark and spooky..."};
+        int expectedLength = expected.length;
+
+        String[] actual = game.getPromptOptions();
+
+        assertTrue(Arrays.equals(expected, actual), "Should match expected");
+        assertTrue(expectedLength == actual.length, "Should contain same amount of elements");
+    }
+
+    @Test
+    void getPromptRepliesTest() {
+        String[] expected =  new String[]{"A large dragon jumps out in front of you! He opens his jaws and... Gobbles you down in one bite!"};
+        int expectedLength = expected.length;
+
+        String[] actual = game.getPromptOptions();
+
+        assertTrue(Arrays.equals(expected, actual), "Should match expected");
+        assertTrue(expectedLength == actual.length, "Should contain same amount of elements");
+    }
+
+    @Test
+    void getInputStreamTest() {
+        ByteArrayInputStream inputText = new ByteArrayInputStream("Test".getBytes());
+        String actual = game.getInputStream(inputText);
+
+        assertTrue(actual.equals("Test"), "Should return input text");
 
     }
 
